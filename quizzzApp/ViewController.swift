f237b6d75a44caf4a0933af63d70aa61c8a742ea
AnasooyaS <sasindrananasooya@gmail.com>
2023-08-21 12:09:19 +0530

    Initial Commit

---
+++ quizzzApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quizzzApp
+//
+//  Created by Anasooya on 04/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
